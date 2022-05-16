727ccbe9f99caa3f1cfc7544dfb17f915002d2b6
Maroua Ghaouat <hm_ghaouat@esi.dz>
2022-05-16 19:56:24 +0200

    changed

--- finetune.py
+++ finetune.py
@@ -5,7 +5,7 @@ from torch.utils.data import DataLoader
 import pandas as pd
 import os, math, sys, logging
 
-def fit_models(num_folder, nb_model,  input_csv_folder, output_model_file, batch, s_bert_model):
+def fit_models(num_folder, nb_model,  input_csv_folder, output_model_file, batch, s_bert_model, validation_data):
     #### Just some code to print debug information to stdout
     logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
@@ -15,54 +15,32 @@ def fit_models(num_folder, nb_model,  input_csv_folder, output_model_file, batch
     train_samples = []
     dev_samples=[]
     logging.info(f"input csv folder {input_csv_folder}")
-    chunk = pd.read_csv(input_csv_folder, names = ['abstract','mesh_pos', 'mesh_neg'], chunksize=750) 
-    cpt=1
+    chunk = pd.read_csv(input_csv_folder, names = ['abstract','mesh_pos', 'mesh_neg'], chunksize=1000) 
+
+    mesh_pos=[]
+    mesh_neg = []
+    abstracts=[]
     for chunk_data in chunk:
-        if (cpt==1):
-            mesh_pos = chunk_data['mesh_pos'].values.tolist()
-            mesh_neg = chunk_data['mesh_neg'].values.tolist()
-            abstracts = chunk_data['abstract'].values.tolist()
-            evaluator = evaluation.TripletEvaluator(abstracts, mesh_pos, mesh_neg)
-            cpt+=1
-            mesh_pos=[]
-            mesh_neg = []
-            abstracts=[]
-            
-        else:
-            mesh_pos.extend(chunk_data['mesh_pos'].values.tolist())
-            mesh_neg.extend(chunk_data['mesh_neg'].values.tolist())
-            abstracts.extend(chunk_data['abstract'].values.tolist())
-            cpt+=1
-        
-        if (cpt==21): break
+        mesh_pos.extend(chunk_data['mesh_pos'].values.tolist())
+        mesh_neg.extend(chunk_data['mesh_neg'].values.tolist())
+        abstracts.extend(chunk_data['abstract'].values.tolist())
+
     for i in range(len(abstracts)) :
                 '''train_samples.append(InputExample(texts=[abstracts[i], mesh_pos[i]], label=0.9))
                 train_samples.append(InputExample(texts=[abstracts[i], mesh_neg[i]], label=0.1))'''
                 train_samples.append(InputExample(texts=[abstracts[i], mesh_pos[i],mesh_neg[i]]))
     print(train_samples)
     loader = DataLoader(train_samples, shuffle=True, batch_size=batch)  
-    
-    '''
-    mesh_pos.extend(mesh_neg)
-    scores=list(itertools.repeat(0.9, len(mesh_neg)))
-    scores.extend(list(itertools.repeat(0.1, len(mesh_neg))))
-    abstracts.extend(abstracts)
-    evaluator = EmbeddingSimilarityEvaluator(abstracts,mesh_pos,scores)
-    '''
-    
-    '''
-    dev_samples.append(abstracts1)
-    dev_samples.append(mesh_pos1)
-    dev_samples.append(scores)
-    
-    dev_samples.append(InputExample(texts=[abstracts for abstracts in abstracts1]))
-    dev_samples.append([InputExample(texts=mesh_pos) for mesh_pos in mesh_pos1])
-    dev_samples.append([InputExample(texts=score) for score in scores])
-    dev_dataloader = DataLoader(dev_samples, shuffle=False, batch_size=batch)
-    dev_data=[]
-    '''
-    
-    
+
+    chunk = pd.read_csv(validation_data, names = ['abstract','mesh_pos', 'mesh_neg'], chunksize=1000) 
+    mesh_pos=[]
+    mesh_neg = []
+    abstracts=[]
+    for chunk_data in chunk:
+        mesh_pos.extend(chunk_data['mesh_pos'].values.tolist())
+        mesh_neg.extend(chunk_data['mesh_neg'].values.tolist())
+        abstracts.extend(chunk_data['abstract'].values.tolist())   
+    evaluator = evaluation.TripletEvaluator(abstracts, mesh_pos, mesh_neg)
         
     epochs = 1
     warmup_steps = math.ceil(len(loader) * epochs / batch_size * 0.1) #10% of train data for warm-up
@@ -99,25 +77,8 @@ output_model_file = './SSciFive/SSciFive_v'
 #output_model_file = 'D:\SSciFive\'
 batch_size = 16
 nb_model = 0
-
-'''
-def main():
-    filenames =  os.listdir(finetuning_data)
-    l = sorted(filenames, key=lambda x: os.path.splitext ('_')[0])
-   
-    if int(sys.argv[1]) == 0 :
-        #bert = 'kamalkraj/bioelectra-base-discriminator-pubmed'
-        bert = 'razent/SciFive-base-Pubmed'
-    else :
-        bert = output_model_file+ str( int(sys.argv[1]) - 1 ) 
-        
-    logging.info("csv file ", int(sys.argv[1]) )
-    fit_models(int(sys.argv[1]), int(sys.argv[1]), finetuning_data + filenames[int(sys.argv[1])], output_model_file, batch_size= batch_size, s_bert_model=bert)
-    
-
-if __name__ == "__main__":
-    main()'''
+validation_data='validation.csv'
 #bert = './SSciFive/1'
 bert='razent/SciFive-base-Pubmed'
-fit_models(0, 2, 'pubmedfile7.csv', output_model_file, batch= 256, s_bert_model=bert)
+fit_models(0, 3, 'all_triples.csv', output_model_file, 128, bert, validation_data)
     
