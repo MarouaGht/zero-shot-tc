6b73affd0aefc131a1b18c2a8460c573f5f93949
Maroua Ghaouat <hm_ghaouat@esi.dz>
2022-05-16 13:01:34 +0200

    wlina l15000

--- finetune.py
+++ finetune.py
@@ -15,7 +15,7 @@ def fit_models(num_folder, nb_model,  input_csv_folder, output_model_file, batch
     train_samples = []
     dev_samples=[]
     logging.info(f"input csv folder {input_csv_folder}")
-    chunk = pd.read_csv(input_csv_folder, names = ['abstract','mesh_pos', 'mesh_neg'], chunksize=5000) 
+    chunk = pd.read_csv(input_csv_folder, names = ['abstract','mesh_pos', 'mesh_neg'], chunksize=750) 
     cpt=1
     for chunk_data in chunk:
         if (cpt==1):
@@ -34,7 +34,7 @@ def fit_models(num_folder, nb_model,  input_csv_folder, output_model_file, batch
             abstracts.extend(chunk_data['abstract'].values.tolist())
             cpt+=1
         
-        #if (cpt==21): break
+        if (cpt==21): break
     for i in range(len(abstracts)) :
                 '''train_samples.append(InputExample(texts=[abstracts[i], mesh_pos[i]], label=0.9))
                 train_samples.append(InputExample(texts=[abstracts[i], mesh_neg[i]], label=0.1))'''
