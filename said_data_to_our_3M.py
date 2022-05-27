76855443f11eb38e7feecfadaaa598c253bfd931
Maroua Ghaouat <hm_ghaouat@esi.dz>
2022-05-27 18:35:24 +0200

    tot

--- said_data_to_our_3M.py
+++ said_data_to_our_3M.py
@@ -27,33 +27,35 @@ def generate_new_triples_from_pairs(dataset, dataset_path):
     pmid_mesh_all_files = sorted(os.listdir(dataset_path+'new_pos_neg'), key = firstBefore_Chars) 
     f_evaluation_data = open(dataset_path + 'training_data1.csv' ,'a')
     writer = csv.writer(f_evaluation_data)
-    for data_line in range(3000000, len(dataset)):
+    for data_line in range(3079175, len(dataset)):
         pmid = dataset[data_line][0]
         for i in range(len(pmid_mesh_all_files)):     
             if  start_index(pmid_mesh_all_files[i] ) <= pmid <= end_index(pmid_mesh_all_files[i] ) :
-                print(pmid)
-                pmid_mesh_all=dataset_path+'new_pos_neg/'+pmid_mesh_all_files[i] 
-                with open(pmid_mesh_all, mode= 'r', encoding="UTF8") as f_pmid_mesh_all:
-                    pmid_mesh = json.load(f_pmid_mesh_all)
-                mesh_neg = pmid_mesh[str(pmid)]['mesh_neg']
-                mesh_pos =  dataset[data_line][2]
-                cpt_mesh_pos =0
-                for mesh in mesh_pos :
-                    if cpt_mesh_pos == 0:
-                        mesh_pos_list =  mesh
-                    else :
-                        mesh_pos_list = mesh_pos_list + ';' + mesh
-                    cpt_mesh_pos +=1
-                cpt_mesh_neg =0       
-                for mesh in mesh_neg :
-                    if cpt_mesh_neg == 0:
-                        mesh_neg_list =  mesh
-                    else :
-                        mesh_neg_list = mesh_neg_list + ';' + mesh
-                    cpt_mesh_neg +=1
-                    if cpt_mesh_neg == cpt_mesh_pos : break
-                writer.writerow([pmid,dataset[data_line][1], mesh_pos_list, mesh_neg_list ])
-                continue
+                try :
+                    print(pmid)
+                    pmid_mesh_all=dataset_path+'new_pos_neg/'+pmid_mesh_all_files[i] 
+                    with open(pmid_mesh_all, mode= 'r', encoding="UTF8") as f_pmid_mesh_all:
+                        pmid_mesh = json.load(f_pmid_mesh_all)
+                    mesh_neg = pmid_mesh[str(pmid)]['mesh_neg']
+                    mesh_pos =  dataset[data_line][2]
+                    cpt_mesh_pos =0
+                    for mesh in mesh_pos :
+                        if cpt_mesh_pos == 0:
+                            mesh_pos_list =  mesh
+                        else :
+                            mesh_pos_list = mesh_pos_list + ';' + mesh
+                        cpt_mesh_pos +=1
+                    cpt_mesh_neg =0       
+                    for mesh in mesh_neg :
+                        if cpt_mesh_neg == 0:
+                            mesh_neg_list =  mesh
+                        else :
+                            mesh_neg_list = mesh_neg_list + ';' + mesh
+                        cpt_mesh_neg +=1
+                        if cpt_mesh_neg == cpt_mesh_pos : break
+                    writer.writerow([pmid,dataset[data_line][1], mesh_pos_list, mesh_neg_list ])
+                    continue
+                except : pass
 
 def main():
     file_path = "./said_data/data1.zip"
